fadc3d8525fb4a9f22256d9587696d23ff7bbf8b
Ameur Youssef <ameuryoussef94@gmail.com>
2023-04-28 20:04:13 +0200

    Double request bug fix

--- OrdinaryJoe/OJ_Blog/static/OJ_Blog/scripts/HomePage/ValidateForm.js
+++ OrdinaryJoe/OJ_Blog/static/OJ_Blog/scripts/HomePage/ValidateForm.js
@@ -19,18 +19,22 @@ if (!window.formElementsLoaded) {
             handleClick(homeFormElements);
         });
 
-        function handleClick(homeFromElements) {
+        function handleClick(homeFromElements)
+        {
 
             let emailInput;
             let activeFrom;
 
-            if (homeFromElements.subEmailInput.value) {
+            if (homeFromElements.subEmailInput.value)
+            {
                 formData = new FormData();
                 formData.append("subscribe-form", "");
                 formData.append("sub-email", homeFromElements.subEmailInput.value);
                 emailInput = homeFromElements.subEmailInput;
                 activeFrom = homeFromElements.subscribeForm;
-            } else if (homeFromElements.msgEmailInput.value) {
+            }
+            else if (homeFromElements.msgEmailInput.value)
+            {
                 formData = new FormData();
                 formData.append("get-in-touch-form", "");
                 formData.append("g-name",  homeFormElements.messengerNameInput.value);
@@ -42,44 +46,48 @@ if (!window.formElementsLoaded) {
 
             const csrftoken = document.getElementsByName("csrfmiddlewaretoken")[0].value
 
-            fetch('/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'X-CSRFToken': csrftoken
-                },
-                body: new URLSearchParams(formData)
-            })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                if (data.error) {
-                    emailInput.classList.add('is-invalid');
-                    // Add code here to display the error message
-                } else {
-                    emailInput.classList.remove('is-invalid');
-                    emailInput.classList.add("succeeded")
-
-                     if (homeFromElements.subEmailInput.value) {
-                        homeFormElements.subEmailInput.value = ''; // clear the input field
-                        const successMessage = document.createElement('p');
-                        successMessage.innerText = 'Thank you for subscribing!';
-                        activeFrom.parentNode.replaceChild(successMessage, homeFormElements.subscribeForm);
-                    } else if (homeFromElements.msgInput.value){
-                        homeFormElements.messengerNameInput.value = "";
-                        homeFromElements.msgEmailInput.value = "";
-                        homeFormElements.msgInput.value = "";
-                        showPopup("Thanks for contacting me. I will get back to you as soon as possible", 3000)
+            if (!homeFromElements.submitting) {
+                homeFromElements.submitting = true; // set the submitting flag
+                fetch('/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'X-CSRFToken': csrftoken
+                    },
+                    body: new URLSearchParams(formData)
+                })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
                     }
-                }
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
+                    return response.json();
+                })
+                .then((data) => {
+                    if (data.error) {
+                        emailInput.classList.add('is-invalid');
+                        // Add code here to display the error message
+                    } else {
+                        emailInput.classList.remove('is-invalid');
+                        emailInput.classList.add("succeeded")
+
+                         if (homeFromElements.subEmailInput.value) {
+                            homeFormElements.subEmailInput.value = ''; // clear the input field
+                            const successMessage = document.createElement('p');
+                            successMessage.innerText = 'Thank you for subscribing!';
+                            activeFrom.parentNode.replaceChild(successMessage, homeFormElements.subscribeForm);
+                        } else if (homeFromElements.msgInput.value){
+                            homeFormElements.messengerNameInput.value = "";
+                            homeFromElements.msgEmailInput.value = "";
+                            homeFormElements.msgInput.value = "";
+                            showPopup("Thanks for contacting me. I will get back to you as soon as possible", 3000)
+                        }
+                    }
+                })
+                .catch((error) => {console.error('Error:', error);})
+                .finally(() => { homeFromElements.submitting = false; });
+            } else {
+                console.log('Form is already being submitted.');
+            }
         }
 
         function showPopup(message, timeout) {
